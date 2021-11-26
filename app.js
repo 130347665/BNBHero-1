913f1f343d7c88fe4932908365dce5ffc7cc1a39
Leo Hsieh <LeoHsieh@ami.com>
2021-11-26 15:16:31 +0800

    aaa

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+window.alert("bb");
 // 引入模組
 const Web3 = require('web3');
 
@@ -5,5 +7,5 @@ const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'));
 //
 const address = '0xb91d555597dF16583F614b37874F40f5ECF150C9';
-window.alert("aaa");
+
 
