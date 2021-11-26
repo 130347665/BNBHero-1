91bae9e56a189b39b7fac01ea205b3d68366e923
Leo Hsieh <LeoHsieh@ami.com>
2021-11-26 14:34:10 +0800

    test

--- app.js
+++ app.js
@@ -2,11 +2,9 @@
 const Web3 = require('web3');
 
 // HTTP provider
-const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/', request_kwargs={'timeout': 60}));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'));
 
 const address = '0xb91d555597dF16583F614b37874F40f5ECF150C9';
-
-const Web3 = require('web3');
 
 // 設定 css inject
 const css = csjs `
