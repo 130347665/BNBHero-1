57f1cbb2c35c960bd8f9e0e2e9cf8404be3db964
Leo Hsieh <LeoHsieh@ami.com>
2021-11-26 15:07:33 +0800

    etets

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 // 引入模組
-//const Web3 = require('web3');
+const Web3 = require('web3');
 
 // HTTP provider
-//const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://bsc-dataseed.binance.org/'));
 //
-//const address = '0xb91d555597dF16583F614b37874F40f5ECF150C9';
-window.alert("ttttt");
+const address = '0xb91d555597dF16583F614b37874F40f5ECF150C9';
+window.alert("aaa");
 
