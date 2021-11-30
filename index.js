ca8e5966df4757c7a55590c180f7bb351dc330d0
Leo Hsieh <LeoHsieh@ami.com>
2021-11-30 14:09:39 +0800

    sss

--- index.js
+++ index.js
@@ -2,13 +2,47 @@
     const loginButton = document.getElementById('loginButton')
     const userWallet = document.getElementById('userWallet')
 
-    window.userAddress = null;
     window.onload = async () => {
-      // Init Web3 connected to ETH network
-      if (window.ethereum) {
-        window.web3 = new Web3(window.ethereum);
-      } else {
-        alert("No ETH brower extension detected.");
-      }
-
+        if (typeof window.ethereum !== 'undefined') {
+            window.web3 = new Web3(window.ethereum);
+            loginButton.addEventListener('click', loginWithMetaMask)
+        } else {
+            alert("MetaMask is NOT installed!");
+            loginButton.setAttribute('disabled', 'disabled');
+        }
     };
+
+    // Login with Web3 via Metamasks window.ethereum library
+    async function loginWithMetaMask() {
+      if (window.web3) {
+        try {
+            // We use this since ethereum.enable() is deprecated. This method is not
+            // available in Web3JS - so we call it directly from metamasks' library
+            const selectedAccount = await window.ethereum
+                .request({
+                  method: "eth_requestAccounts",
+                })
+                .then((accounts) => accounts[0])
+                .catch(() => {
+                  throw Error("No account selected!");
+                });
+            window.userWalletAddress = selectedAccount;
+            window.localStorage.setItem("userWalletAddress", selectedAccount);
+        } catch (error) {
+            console.error(error);
+        }
+        } else {
+            alert("MetaMask is NOT installed!");
+        }
+    }
+
+    function logout() {
+        window.userWalletAddress = null;
+        window.localStorage.removeItem("userWalletAddress");
+    }
+
+    async function getContractSymbol() {
+
+        alert(window.web3.eth.net.getNetworkType());
+
+    }
