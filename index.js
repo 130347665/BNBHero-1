acdec870fc1f0a9355f3f8eb19f5c382a5b9c954
Leo Hsieh <LeoHsieh@ami.com>
2021-11-30 13:26:33 +0800

    zzz

--- index.js
+++ index.js
@@ -1,51 +1,14 @@
     window.userWalletAddress = null
     const loginButton = document.getElementById('loginButton')
     const userWallet = document.getElementById('userWallet')
-    const userWallet2 = document.getElementById('userWallet2')
 
-    function toggleButton() {
-      if (!window.ethereum) {
-        loginButton.innerText = 'MetaMask is not installed'
-        loginButton.classList.remove('bg-purple-500', 'text-white')
-        loginButton.classList.add('bg-gray-500', 'text-gray-100', 'cursor-not-allowed')
-        return false
+    window.userAddress = null;
+    window.onload = async () => {
+      // Init Web3 connected to ETH network
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+      } else {
+        alert("No ETH brower extension detected.");
       }
 
-      loginButton.addEventListener('click', loginWithMetaMask)
-    }
-
-    async function loginWithMetaMask() {
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
-        .catch((e) => {
-          console.error(e.message)
-          return
-        })
-      if (!accounts) { return }
-
-      window.userWalletAddress = accounts[0]
-      window.userWalletAddress2 = accounts[1]
-      userWallet.innerText = window.userWalletAddress
-      userWallet2.innerText = accounts.length
-      loginButton.innerText = 'Sign out of MetaMask'
-
-      loginButton.removeEventListener('click', loginWithMetaMask)
-      setTimeout(() => {
-        loginButton.addEventListener('click', signOutOfMetaMask)
-      }, 200)
-    }
-
-    function signOutOfMetaMask() {
-      window.userWalletAddress = null
-      userWallet.innerText = ''
-      userWallet2.innerText = ''
-      loginButton.innerText = 'Sign in with MetaMask'
-
-      loginButton.removeEventListener('click', signOutOfMetaMask)
-      setTimeout(() => {
-        loginButton.addEventListener('click', loginWithMetaMask)
-      }, 200)
-    }
-
-    window.addEventListener('DOMContentLoaded', () => {
-      toggleButton()
-    });
+    };
