5d9901cdf21473ea31bfcd4e67a86ae023ca0a8e
Leo Hsieh <LeoHsieh@ami.com>
2021-11-30 10:17:04 +0800

    aaa

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ window.onload = function() {
       if (typeof web3 !== 'undefined') {
         alert('bb');
         web3 = new Web3(web3.currentProvider);
-        
-var message = "Some string"
-var hash = web3.utils.sha3(message)
-var accounts = await web3.eth.getAccounts()
-alert(accounts[0]);
-var signature = await web3.eth.personal.sign(hash, accounts[0])
+        alert('cc');
       } else {
         alert('dd');
         web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io"));
