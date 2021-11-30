4a616fb9e882cc5c13cfbde500d1153961068db9
Leo Hsieh <LeoHsieh@ami.com>
2021-11-30 15:16:33 +0800

    ddd

--- index.js
+++ index.js
@@ -44,7 +44,7 @@
     }
 
     async function getContractSymbol() {
-        console.error(window.chainId);
+        console.log(window.chainId);
         if (window.chainId == 56) {
             alert("MetaMask is  installed!");
         }
