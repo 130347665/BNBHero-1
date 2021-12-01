4d9237ab0b2c22035a87770c56d643b6c8911acc
Leo Hsieh <LeoHsieh@ami.com>
2021-12-01 12:30:24 +0800

    aaa

--- index.js
+++ index.js
@@ -64,7 +64,7 @@
                 html = html + '<img style="width: 200px;" src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" data-preview-src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" class="rounded img-thumbnail card-img-top">';
                 html = html + '<h5 class="card-title text-center">NFT #' + heros[i][7] + '</h5>'
                 html = html + '<div class="progress"><div class="progress-bar bg-danger text-center text-dark" role="progressbar" style="width: ' + heros[i][6]/10 + '%;" aria-valuenow="' + heros[i][6] + '" aria-valuemin="0" aria-valuemax="100">' + heros[i][6] + '</div></div>'
-                html = html + '<button type="button" class="btn btn-primary btn-lg btn-block">Fight</button>'
+                html = html + '<button type="button" class="mx-auto btn btn-primary btn-sm">Fight</button>'
                 html = html + '</div></div>'
 
             }
