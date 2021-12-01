a005ff7b994913f0f93268b2212b17b36d35ed10
Leo Hsieh <LeoHsieh@ami.com>
2021-12-01 16:49:03 +0800

    aaa

--- index.js
+++ index.js
@@ -82,22 +82,12 @@
                 html = html + '<img style="width: 200px;" src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" data-preview-src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" class="rounded img-thumbnail card-img-top">';
                 html = html + '<h5 class="card-title text-center">NFT #' + heros[i][7] + '</h5>';
                 html = html + '<div class="progress"><div class="progress-bar bg-danger text-center text-dark" role="progressbar" style="width: ' + heros[i][6]/10 + '%;" aria-valuenow="' + heros[i][6] + '" aria-valuemin="0" aria-valuemax="100">' + heros[i][6] + '</div></div>';
-
-                html = html + '<button style="width: 100%;" type="button" onclick="fight(' + heros[i][7];
-                if (exp == 999 || heros[i][6] < 200) {
-                    html = html + ')" class="btn btn-secondary" disabled>Fight</button>';
+                if (exp != 999 || heros[i][6] >= 200) {
+                    html = html + '<button style="width: 100%;" type="button" onclick="fight(' + heros[i][7] + ')"class="btn btn-danger">Fight</button>';
                 }
-                else
-                {
-                    html = html + ')"class="btn btn-danger">Fight</button>';
-                }
-                html = html + '<button style="width: 100%;" type="button" onclick="unLockLevel(' + heros[i][7];
+                
                 if (exp == 999) {
-                    html = html + ')"class="btn btn-warning">Unlock Level</button>';
-                }
-                else
-                {
-                    html = html + ')" class="btn btn-secondary" disabled>Unlock Level</button>';
+                    html = html + '<button style="width: 100%;" type="button" onclick="unLockLevel(' + heros[i][7] + ')"class="btn btn-warning">Unlock Level</button>';
                 }
                 html = html + '</div></div>'
 
