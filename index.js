8e299e587f68e825d43b8961914b5315bcb4ce9f
Leo Hsieh <LeoHsieh@ami.com>
2021-12-01 16:29:15 +0800

    aaa

--- index.js
+++ index.js
@@ -83,7 +83,7 @@
                 html = html + '<h5 class="card-title text-center">NFT #' + heros[i][7] + '</h5>';
                 html = html + '<div class="progress"><div class="progress-bar bg-danger text-center text-dark" role="progressbar" style="width: ' + heros[i][6]/10 + '%;" aria-valuenow="' + heros[i][6] + '" aria-valuemin="0" aria-valuemax="100">' + heros[i][6] + '</div></div>';
                 html = html + '<div class="btn-group btn-group-sm" role="group">';
-                html = html + '<button type="button" class="btn btn-primary" onclick="fight(' + heros[i][7];
+                html = html + '<button type="button" class="btn btn-secondary" onclick="fight(' + heros[i][7];
                 if (exp == 999 || heros[i][6] < 200) {
                     html = html + ')" disabled>Fight</button>';
                 }
@@ -91,7 +91,7 @@
                 {
                     html = html + ')">Fight</button>';
                 }
-                html = html + '<button type="button" class="btn btn-primary" onclick="unLockLevel(' + heros[i][7];
+                html = html + '<button type="button" class="btn btn-secondary" onclick="unLockLevel(' + heros[i][7];
                 if (exp == 999) {
                     html = html + ')">Unlock Level</button>';
                 }
