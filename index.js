d7b35d3e5755b2425eac1e9908167c0f6904e1c8
Leo Hsieh <LeoHsieh@ami.com>
2021-12-01 16:11:59 +0800

    sss

--- index.js
+++ index.js
@@ -49,7 +49,7 @@
         if (!address) {
             return "";
         }
-        return address.substr(0, 5)} + '...' + address.substr(
+        return address.substr(0, 5) + '...' + address.substr(
             address.length - 5,
             address.length
         );
