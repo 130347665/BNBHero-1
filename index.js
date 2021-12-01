3f2e405b9226fd3793cf0d64053c7f2a4d504110
Leo Hsieh <LeoHsieh@ami.com>
2021-12-01 10:44:59 +0800

    aaa

--- index.js
+++ index.js
@@ -63,8 +63,8 @@
                 html = html + '<div class="card" style="width: 200px;"><div class="card-body">'
                 html = html + '<img src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" data-preview-src="https://play.bnbheroes.io/cards/' + heros[i][0] + '.png" class="float-left rounded img-thumbnail card-img-top">';
                 html = html + '<h5 class="card-title text-center">NFT #' + heros[i][7] + '</h5>'
-                html = html + '<div class="progress"><div class="progress-bar bg-danger text-center text-dark" role="progressbar" style="width: ' + heros[i][6]/10 + '%;" aria-valuenow="' + heros[i][6] + '" aria-valuemin="0" aria-valuemax="1000">' + heros[i][6] + '</div></div>'
-                html = html + '</div></div>'
+                html = html + '<div class="progress"><div class="progress-bar bg-danger text-center text-dark" role="progressbar" style="width: ' + heros[i][6]/10 + '%;" aria-valuenow="' + heros[i][6] + '" aria-valuemin="0" aria-valuemax="100">' + heros[i][6] + '</div></div>'
+                html = html + '</div>' + heros[i][6] + '</div>'
 
             }
             document.getElementById("userHero").innerHTML = html;
